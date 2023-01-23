03028047d39446147854dbe30fe70e6241541691
Thanatporn Lawsirirattana <nthalaw21@gmail.com>
2023-01-24 00:42:54 +0700

    feat: finish create reply comment on level1 and render the result

--- src/features/Comment.js
+++ src/features/Comment.js
@@ -1,11 +1,24 @@
 import React, { useState } from 'react';
 import dateFormat from 'dateformat';
-import { getReplyByCommentId } from '../api/graphApi';
+import { createReply, getReplyByCommentId } from '../api/graphApi';
 import Reply from './Reply';
 
 function Comment({ item }) {
   const [replies, setReplies] = useState([]);
   const [toggle, setToggle] = useState(false);
+  const [newMessage, setNewMessage] = useState('');
+  const handleSubmit = async () => {
+    if (!newMessage.trim()) {
+      setNewMessage('');
+      return;
+    }
+    console.log('id', item.id);
+    console.log('newMessage', newMessage);
+    const res = await createReply(String(item.id), newMessage);
+    console.log(res);
+    setNewMessage('');
+    await getReply(item.id);
+  };
 
   const getReply = async (commentId) => {
     console.log(commentId);
@@ -21,9 +34,9 @@ function Comment({ item }) {
       className="bg-slate-200 rounded p-2 text-black flex flex-row justify-between items-center"
     >
       <div>
-        <div key={item.from.id}>{item.from.name} </div>
+        <div key={item.from.id}>Name: {item.from.name} </div>
         <div className="flex gap-5">
-          <div>{item.message} </div>
+          <div>Message: {item.message} </div>
           <div>{dateFormat(item.created_time, 'mmm d, yyyy HH:MM')}</div>
         </div>
         <div className="flex space-x-3">
@@ -48,7 +61,23 @@ function Comment({ item }) {
               </div>
             );
           })}
+        {/* add input here */}
+
+        <div className="flex mt-2">
+          <div className="text-blue-500 mr-2">reply</div>
+          <input
+            className="pl-1"
+            value={newMessage}
+            onChange={(e) => {
+              setNewMessage(e.target.value);
+            }}
+          />
+          <div className="bg-gray-500 px-1" onClick={handleSubmit}>
+            <i className="fa-solid fa-paper-plane text-gray-100 h-5"></i>
+          </div>
+        </div>
       </div>
+
       <div
         onClick={(e) => {
           e.stopPropagation();
--- src/features/Reply.js
+++ src/features/Reply.js
@@ -18,32 +18,34 @@ function Reply({ item, getReply }) {
     await getReply(item.id);
   };
   return (
-    <div className="my-3 p-2 px-5 rounded bg-gray-300 text-black">
+    <div className="my-3 p-2 px-5 rounded bg-slate-300 text-black">
       <div>
         <div>Name: {item.from.name}</div>
         <div>Message: {item.message}</div>
-        <div
-          className="text-blue-600"
-          onClick={() => {
-            setToggle((toggle) => !toggle);
-          }}
-        >
-          reply
-        </div>
-        {toggle && (
-          <div className="flex">
-            <input
-              className="pl-1"
-              value={newMessage}
-              onChange={(e) => {
-                setNewMessage(e.target.value);
-              }}
-            />
-            <div className="bg-gray-500 px-1" onClick={handleSubmit}>
-              <i className="fa-solid fa-paper-plane text-gray-100 h-5"></i>
-            </div>
+        <div className="flex mt-1">
+          <div
+            className="text-blue-600 mr-2"
+            onClick={() => {
+              setToggle((toggle) => !toggle);
+            }}
+          >
+            reply
           </div>
-        )}
+          {toggle && (
+            <div className="flex">
+              <input
+                className="pl-1"
+                value={newMessage}
+                onChange={(e) => {
+                  setNewMessage(e.target.value);
+                }}
+              />
+              <div className="bg-gray-500 px-1" onClick={handleSubmit}>
+                <i className="fa-solid fa-paper-plane text-gray-100 h-5"></i>
+              </div>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
